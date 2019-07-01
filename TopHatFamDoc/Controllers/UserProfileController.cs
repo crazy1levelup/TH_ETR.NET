4d7053458fff39298d2aedff38097fde98d18fb1
crazy1levelup <trif.eduard@yahoo.ro>
2019-07-01 18:18:55 +0300

    added put method for user profile

--- TopHatFamDoc/Controllers/UserProfileController.cs
+++ TopHatFamDoc/Controllers/UserProfileController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
@@ -15,10 +16,12 @@ namespace TopHatFamDoc.Controllers
     public class UserProfileController : ControllerBase
     {
         private UserManager<ApplicationUser> _userManager;
+        private readonly AuthenticationContext _context;
 
-        public UserProfileController(UserManager<ApplicationUser> userManager)
+        public UserProfileController(UserManager<ApplicationUser> userManager, AuthenticationContext context)
         {
             _userManager = userManager;
+            _context = context;
         }
 
         [HttpGet]
@@ -35,6 +38,27 @@ namespace TopHatFamDoc.Controllers
                 user.ZiuaNastere,
                 user.DataInregistrare,
                 user.Notes
+
+            };
+
+        }
+
+        [HttpPut]
+        [Authorize]
+        //PUT: /api/UserProfile
+        public async Task<Object> PutUserProfile([FromBody] ApplicationUserModel userProfile)
+        {   
+            string userId = User.Claims.First(c => c.Type == "UserID").Value;
+            var user = await _userManager.FindByIdAsync(userId);
+            user.Notes = userProfile.Notes;
+            await _context.SaveChangesAsync();
+            return new
+            {
+                user.Nume,
+                user.Prenume,
+                user.ZiuaNastere,
+                user.DataInregistrare,
+                user.Notes,
 
             };
 
